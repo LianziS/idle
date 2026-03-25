bbe98a59ef23fb0ae58a45b07a612b50c6508ff1
uncle-jvp <uncle-jvp@users.noreply.github.com>
2026-03-25 15:04:38 +0800

    修复：updateUI 中添加 renderPotionsInventory 调用

--- game.js
+++ game.js
@@ -1967,6 +1967,7 @@ function updateUI() {
     renderPlanksInventory();
     renderIngotsInventory();
     renderFabricsInventory();
+    renderPotionsInventory();
     renderToolsInventory();
 }
 
